e8288ab01b2a835a17469317751a40255b1f1178
Leo Zarazaga <lza1313@outlook.com>
2024-01-16 10:31:15 +0100

    Disable csrf in the security filter chain

--- community-blog/src/main/java/com/communityblog/controller/BlogUserController.java
+++ community-blog/src/main/java/com/communityblog/controller/BlogUserController.java
@@ -5,7 +5,9 @@ import com.communityblog.service.BlogUserService;
 import jakarta.validation.Valid;
 import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,33 +26,23 @@ public class BlogUserController {
         this.blogUserService = blogUserService;
     }
 
+
     @PostMapping("/register")
     public ResponseEntity<RegisterSuccess> registerUser(@Valid @RequestBody UserDTO userDTO) {
         var result = blogUserService.register(userDTO.username(), userDTO.password(), userDTO.email());
-        return ResponseEntity.ok().body(result);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 
+    @GetMapping("/success")
+    public String success(){
+        return "Register successful!";
+    }
 
-   /* @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@Valid @RequestBody UserDTO userDTO) {
-        //THIS IS JUST AN EXAMPLE CODE. NOT FOR DEVELOPER USER!
-        try {
-            Role roleEnum = (userDTO.role() != null) ? Role.valueOf(userDTO.role().toString()) : null;
-
-            if (roleEnum != null) {
-                var result = blogUserService.register(userDTO.username(), userDTO.password(), userDTO.email(), roleEnum.toString());
-                return ResponseEntity.ok().body("Registration successful");
-            } else {
-                // Handle the case where the role is null
-                return ResponseEntity.badRequest().body("Role cannot be null");
-            }
-        } catch (IllegalArgumentException e) {
-            // Handle invalid role string, for example, log an error or return a specific response
-            return ResponseEntity.badRequest().body("Invalid role provided");
-        }
-    }*/
-
+    @GetMapping("/hello")
+    public String getMessage(){
+        return "Hello Lina!";
+    }
 
 
 }
--- community-blog/src/main/java/com/communityblog/security/SecurityConfig.java
+++ community-blog/src/main/java/com/communityblog/security/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.communityblog.security;
 
 import org.springframework.context.annotation.Bean;
-/* import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
 public class SecurityConfig {
     @Bean
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .requestMatchers("/register").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf(csrf -> csrf.disable());  // Disable CSRF for simplicity in this example
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        //.requestMatchers("/hello").authenticated()
+                        .requestMatchers("/register", "/success").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+        return http.build();
+
     }
-}*/
+}
+
+
