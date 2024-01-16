a55bcdb598aa098610f121494ef834b52392257a
liina <liina.oliin@gmail.com>
2024-01-16 10:58:02 +0100

    added Bcrypt to passwords

--- community-blog/src/main/java/com/communityblog/security/SecurityConfig.java
+++ community-blog/src/main/java/com/communityblog/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,13 +17,20 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         //.requestMatchers("/hello").authenticated()
-                        .requestMatchers("/register", "/success").permitAll()
+                        .requestMatchers("/register", "/success", "/").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults());
         return http.build();
 
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
 
 
--- community-blog/src/main/java/com/communityblog/service/BlogUserService.java
+++ community-blog/src/main/java/com/communityblog/service/BlogUserService.java
@@ -5,6 +5,7 @@ import com.communityblog.exception.RegistrationFailureException;
 import com.communityblog.model.BlogUser;
 import com.communityblog.repository.BlogUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,14 +13,19 @@ import java.util.List;
 @Service
 public class BlogUserService {
     private final BlogUserRepository blogUserRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public BlogUserService(BlogUserRepository blogUserRepository) {
+    public BlogUserService(BlogUserRepository blogUserRepository, PasswordEncoder passwordEncoder) {
         this.blogUserRepository = blogUserRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public RegisterSuccess register(String username, String password, String email ) {
         BlogUser newBlogUser = new BlogUser(username, password, email, "USER");
+        String hashPassword =passwordEncoder.encode(password);
+        newBlogUser.setPassword(hashPassword);
+
         try {
             blogUserRepository.save(newBlogUser);
             return new RegisterSuccess("Account with username: {" + username + "} Registered successfully");
@@ -28,8 +34,6 @@ public class BlogUserService {
             throw new RegistrationFailureException("Registration failed: " + e.getMessage());
         }
     }
-
-
 
 }
 
