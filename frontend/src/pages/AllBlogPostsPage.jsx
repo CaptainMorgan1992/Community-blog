a47a07c842e85a5ba4c2fc29ae1b255559051621
Rufat-J <rj.7@live.se>
2024-01-21 11:13:33 +0100

    searchbar

--- frontend/src/pages/AllBlogPostsPage.jsx
+++ frontend/src/pages/AllBlogPostsPage.jsx
@@ -1,21 +1,35 @@
 import GlobalContext from "../GlobalContext.jsx";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {Link} from "react-router-dom";
+export default function AllBlogPostsPage() {
+    const { blogPosts } = useContext(GlobalContext);
+    const [searchTerm, setSearchTerm] = useState("");
 
-export default function AllBlogPostsPage () {
-    const {blogPosts} = useContext(GlobalContext)
-    return<>
-        <div className={"blogpost"}>
-            {blogPosts.map((post) => (
-                <div key={post.id}>
-                    <Link to={`/blog/${post.id}`}>
-                        <h2>{post.title}</h2>
-                    </Link>
-                    <p>{post.content}</p>
-                    <p>Author: {post.author}</p>
-                </div>
-            ))}
-        </div>
-    </>
+    const filteredBlogPosts = blogPosts.filter((post) =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
+    return (
+        <>
+            <div className="searchBar">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+            <div className={"blogpost"}>
+                {filteredBlogPosts.map((post) => (
+                    <div key={post.id}>
+                        <Link to={`/blog/${post.id}`}>
+                            <h2>{post.title}</h2>
+                        </Link>
+                        <p className={"content"}>{post.content}</p>
+                        <p>Author: {post.author}</p>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
