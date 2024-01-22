0001191c16e076bffbb7be9c9196f129f2e90dad
liina <liina.oliin@gmail.com>
2024-01-22 14:10:11 +0100

    fixed csrf and login and logout

--- frontend/src/GlobalContext.jsx
+++ frontend/src/GlobalContext.jsx
@@ -6,35 +6,21 @@ const GlobalContext = createContext(null);
 // eslint-disable-next-line react/prop-types
 export const GlobalProvider = ({children}) =>  {
 
-    //useStates for all variables
     const [blogPosts, setBlogPosts] = useState([])
     const [validateResponse, setValidateResponse] = useState(false)
     const [individualPost, setIndividualPost] = useState(null);
-    //imports from database
 
     useEffect(() => {
         setValidateResponse(validateResponse);
-        //setBlogPosts(blogPosts)
         void loadBlogPosts()
     }, [validateResponse]);
 
-
-
- /*   const loadBlogPosts = async () => {
-        await fetch("http://localhost:8080/api/blogpost/all")
-            .then(response => response.json())
-            .then(result => setBlogPosts(result))
-            .catch(error => console.error(error))
-
-    }*/
 
     const submitLogin = async (username, password) => {
         try {
             // Fetch CSRF token
             const csrfRes = await fetch("http://localhost:8080/csrf", { credentials: "include" });
             const token = await csrfRes.json();
-
-            // Submit login request
             const response = await fetch("http://localhost:8080/api/login", {
                 method: 'POST',
                 headers: {
@@ -53,10 +39,16 @@ export const GlobalProvider = ({children}) =>  {
 
     const handleLogout = async () => {
         try {
+            const csrfRes = await fetch("http://localhost:8080/csrf", { credentials: "include" });
+            const token = await csrfRes.json();
+
             await fetch("http://localhost:8080/api/logout", {
                 method: 'POST',
-                headers: {'Content-Type': 'application/json'}
-            })
+                headers: {'Content-Type': 'application/json',
+                'X-CSRF-TOKEN' : token.token
+            },
+                credentials: 'include' });
+
             setValidateResponse(false);
             console.log(validateResponse)
         }
@@ -67,13 +59,11 @@ export const GlobalProvider = ({children}) =>  {
     }
 
     const loadBlogPosts = async () => {
-      // const csrfRes = await fetch("http://localhost:8080/csrf", {credentials: "include"});
-      //const token = await csrfRes.json()
+
         const requestOptions = {
-            method: 'GET', // or any other HTTP method you want to use
+            method: 'GET',
             headers: {
-                'Content-Type': 'application/json' // Example header, adjust as needed
-              //'X-CSRF-TOKEN': token.token
+                'Content-Type': 'application/json'
             },
         };
 
@@ -92,13 +82,11 @@ export const GlobalProvider = ({children}) =>  {
     };
 
     const loadIndividualPost = useCallback(async (postId) => {
-        //const csrfRes = await fetch("http://localhost:8080/csrf", { credentials: "include" });
-        //const token = await csrfRes.json();
+
         const requestOptions = {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
-                //'X-CSRF-TOKEN': token.token
             },
         };
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,14 +8,12 @@ export default function Navbar() {
     return (
         <nav className={'navbar'}>
             {!validateResponse && (
-                // If validateResponse is false, show Login button
                 <Link to={'login'}>
                     <button>Login</button>
                 </Link>
             )}
 
             {validateResponse && (
-                // If validateResponse is true, show Logout, My posts, and Create post buttons
                 <>
                     <Link to={'logout'}>
                         <button onClick={handleLogout}>Logout</button>
@@ -29,13 +27,11 @@ export default function Navbar() {
                 </>
             )}
 
-            {/* Always show the "All posts" button */}
             <Link to={'allPosts'}>
                 <button>All posts</button>
             </Link>
 
             {!validateResponse && (
-                // If validateResponse is false, show Register and Contact us buttons
                 <>
                     <Link to={'register'}>
                         <button>Register</button>
