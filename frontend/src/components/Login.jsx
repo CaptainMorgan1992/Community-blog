cbab4ae6688b93e63f73e474c3a9cea63f1a9d3b
CaptainMorgan1992 <kimberly.morgan1992@outlook.com>
2024-01-22 09:59:54 +0100

    Working on login component

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,49 @@
+import {useNavigate} from "react-router-dom";
+import GlobalContext from "../GlobalContext.jsx";
+import {useContext, useEffect, useState} from "react";
+
+export default function Login() {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [message, setMessage] = useState('')
+    const [validateResponse, setValidateResponse] = useState(false)
+    const {submitLogin} = useContext(GlobalContext)
+    const nav = useNavigate();
+
+
+    useEffect(() => {
+        if (validateResponse) {
+            nav('/');
+        }
+    }, [validateResponse, nav]);
+
+    const checkCredentials = async (e) => {
+        e.preventDefault();
+        if (!username || !password) {
+            setMessage("You must enter your correct login details")
+            return;
+        }
+        const response = await submitLogin(username, password);
+
+        if (response.ok) {
+            setValidateResponse(true);
+            nav('/');
+        }
+    }
+
+    return  <form onSubmit={checkCredentials}>
+        <label htmlFor="username-input">Username</label>
+        <input type="username-input"
+               id="username-input"
+               value={username}
+               onChange={e => setUsername(e.target.value)}/>
+        <label htmlFor="password-input">Password</label>
+        <input type="password"
+               id="password-input"
+               value={password}
+               onChange={e => setPassword(e.target.value)}/>
+        <p>{message}</p>
+        <button id="login-button">Sign in</button>
+    </form>
+}
